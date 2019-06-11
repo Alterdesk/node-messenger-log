86b324820fe838141b820078490d32392e48c713
jurriaan <jurriaan@alterdesk.com>
2019-06-11 12:23:54 +0200

    Logs all arguments and is able to log with or without timestamp

--- src/logger.js
+++ src/logger.js
@@ -1,3 +1,5 @@
+const LOG_TIMESTAMP = process.env.NODE_MESSENGER_LOG_TIMESTAMP || 0;
+
 class Logger {
 
     constructor(logLevel) {
@@ -9,26 +11,47 @@ class Logger {
 
     debug(msg) {
         if(this.logDebug) {
-            console.log("DEBUG", msg);
+            console.log(this.getTag("DEBUG"), this.argumentsToString(arguments));
         }
     }
 
     info(msg) {
         if(this.logInfo) {
-            console.log("INFO", msg);
+            console.log(this.getTag("INFO"), this.argumentsToString(arguments));
         }
     }
 
     warn(msg) {
         if(this.logWarning) {
-            console.warn("WARNING", msg);
+            console.warn(this.getTag("WARNING"), this.argumentsToString(arguments));
         }
     }
 
     error(msg) {
         if(this.logError) {
-            console.error("ERROR", msg);
+            console.error(this.getTag("ERROR"), this.argumentsToString(arguments));
         }
+    }
+
+    getTag(logLevel) {
+        if(LOG_TIMESTAMP === 1) {
+            return "[ " + new Date().toJSON() + " " + logLevel + " ]";
+        }
+        return "[ " + logLevel + " ]";
+    }
+
+    argumentsToString(args) {
+        var string = "";
+        if(!args || args.length === 0) {
+            if(string.length > 0) {
+                string += " ";
+            }
+            return string;
+        }
+        for(let index in args) {
+            string += args[index];
+        }
+        return string;
     }
 
 }
